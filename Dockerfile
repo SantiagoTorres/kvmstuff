0036434d293a6b62ac027fcfa86ba054fe924322
Santiago Torres <santiagotorres@purdue.edu>
2021-10-31 15:36:52 -0400

    enh: download image if not found in the volume

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:latest
 
-RUN apk add qemu-img qemu-system-x86_64 qemu-modules
+RUN apk add qemu-img qemu-system-x86_64 qemu-modules curl
 ADD entrypoint.sh /usr/bin/entrypoint.sh
 
 ENTRYPOINT '/usr/bin/entrypoint.sh'
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -e
-RAM_SIZE=1G
+RAM_SIZE=2G
 IMAGE_PATH=/vm/worker.img
-qemu-system-x86_64 -net nic,model=rtl8139 $PORTMAP -m $RAM_SIZE -smp 2 -enable-kvm -k en-us -vnc :0 -hda $IMAGE_PATH -nographic
+IMAGE_SRC="https://gitlab.archlinux.org/archlinux/arch-boxes/-/jobs/38089/artifacts/raw/output/Arch-Linux-x86_64-basic-20211030.38089.qcow2"
+
+if [ ! -f "$IMAGE_PATH" ]
+then
+    echo "No image found, downloading latest worker image"
+    curl "$IMAGE_SRC" -o "$IMAGE_PATH"
+fi
+qemu-system-x86_64 -net nic,model=rtl8139 $PORTMAP -m $RAM_SIZE -smp 8 -enable-kvm -k en-us -vnc :0 -hda $IMAGE_PATH -nographic
